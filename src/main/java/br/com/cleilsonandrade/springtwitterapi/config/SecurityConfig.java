5750988d9930488785f7ba816ec474c6bce8fdd9
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-03-13 22:21:30 -0300

    ✨ feat: jwt decoder and encoder

--- src/main/java/br/com/cleilsonandrade/springtwitterapi/config/SecurityConfig.java
+++ src/main/java/br/com/cleilsonandrade/springtwitterapi/config/SecurityConfig.java
@@ -10,7 +10,16 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 
 @Configuration
 @EnableWebSecurity
@@ -30,5 +39,18 @@ public class SecurityConfig {
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
     return http.build();
+  }
+
+  @Bean
+  public JwtDecoder jwtDecoder() {
+    return NimbusJwtDecoder.withPublicKey(publicKey).build();
+  }
+
+  @Bean
+  public JwtEncoder jwtEncoder() {
+    JWK jwk = new RSAKey.Builder(this.publicKey).privateKey(privateKey).build();
+    var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+
+    return new NimbusJwtEncoder(jwks);
   }
 }
