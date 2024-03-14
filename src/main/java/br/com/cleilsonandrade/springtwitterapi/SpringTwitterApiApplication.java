7604d1c113dd48891f45c1c52cf6fe049994baf3
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-03-13 21:12:15 -0300

    ✨ feat: first commit

---
+++ src/main/java/br/com/cleilsonandrade/springtwitterapi/SpringTwitterApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.cleilsonandrade.springtwitterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTwitterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTwitterApiApplication.class, args);
+	}
+
+}
